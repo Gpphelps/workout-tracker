cfe95b026fc160e5bc7757cb35ea7d201f3ce731
Gene Phelps <gpphelpsii@gmail.com>
2021-06-15 21:25:41 -0400

    Redid my schema

--- models/workout.js
+++ models/workout.js
@@ -3,10 +3,34 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
     exercises: {
-        type: Array,
-        default: [],
-        require: true
-    },
+            type: {
+                type: String,
+                enum: ["resistance", "cardio"],
+                required: "Please enter exercise's type."
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Please enter exercise's name."
+            },
+            duration: {
+                type: Number,
+                required: "Please enter exercise's duration"
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+
+        },
     day: {
         type: Date,
         default: Date.now
